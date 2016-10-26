b06e847700358b22abb37b7ab4bd754a0ec4635f
Zhizhou Tian <zhizhou.tian@spreadtrum.com>
2016-10-26 10:54:34 +0800

    compress and decompress Image to zImage with zstd on arm32 platform
    Change-Id: Ie4c8ee19f5bc1ef7999173bc6b87258c5cf99e9d
    Signed-off-by: Zhizhou Tian <zhizhou.tian@spreadtrum.com>

--- arch/arm/boot/compressed/decompress.c
+++ arch/arm/boot/compressed/decompress.c
@@ -55,6 +55,10 @@ extern char * strstr(const char * s1, const char *s2);
 #include "../../../../lib/decompress_unlz4.c"
 #endif
 
+#ifdef CONFIG_KERNEL_ZSTD
+#include "../../../../lib/decompress_unzstd.c"
+#endif
+
 int do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x))
 {
 	return __decompress(input, len, NULL, NULL, output, 0, NULL, error);
