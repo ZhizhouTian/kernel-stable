49640bea66626fc6c141ceb097ea3ac8acae89a3
Zhizhou Tian <zhizhou.tian@spreadtrum.com>
2016-12-09 14:43:52 +0800

    porting zstd origin codes to linux with linux coding style
    Change-Id: I4d6943751e6128397c3b9959a93015e0e18e45da
    Signed-off-by: Zhizhou Tian <zhizhou.tian@spreadtrum.com>

---
+++ drivers/misc/zstd/utils.c
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+void test_read32(struct seq_file* m)
+{
+	char x[] = {1, 0, 0, 0};
+	int v = read32(x);
+	seq_printf(m, "%d.\n", v);
+}
+
+int is_little_endian(void)
+{
+	const union {int i; char x[4];} one = {1};
+	return one.x[0];
+}
+
+void test_is_little_endian(struct seq_file* m)
+{
+	seq_printf(m, "is little endian? %s.\n",
+			is_little_endian()?"yes":"no");
+}
